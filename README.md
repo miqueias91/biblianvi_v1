2b53c8742b82dc59016374073b8598dec9197247
Miquéias Matias Caetano <miqueiasmcaetano@gmail.com>
2021-10-25 21:10:03 -0300

    Ok

---
+++ README.md
@@ -0,0 +1,3 @@
+# Holy Bible NIV
+
+The New International Version (NIV) is one of the most recent translations of the Bible. It was published by the International Bible Society.# Bíblia NVI - V1
---
+++ platforms/android/app/build/generated/source/buildConfig/debug/com/phonegap/teindico/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.phonegap.teindico;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.phonegap.teindico";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 10038;
+  public static final String VERSION_NAME = "1.0.38";
+}
